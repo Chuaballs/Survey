e3bf539165ea3903a2eec8714003486fd87c18cc
TitusK98 <tometo5546@gmail.com>
2019-10-24 17:09:34 +0800

    titus

--- resource/php/class/insert.php
+++ resource/php/class/insert.php
@@ -5,11 +5,21 @@ require_once $_SERVER['DOCUMENT_ROOT'].'/survey/resource/db/db.php';
 class insert extends db{
     public $name;
     public $rate;
+    public $RB1;
+    public $RB2;
+    public $RB3;
+    public $RB4;
+    public $RB5;
     public $date;
 
-public function __construct($name=null,$rate=null,$date=null){
+public function __construct($name=null,$rate=null,$RB1=null,$RB2=null,$RB3=null,$RB4=null,$RB5=null,$date=null){
   $this->name = $name;
   $this->rate = $rate;
+  $this->RB1 = $RB1;
+  $this->RB2 = $RB2;
+  $this->RB3 = $RB3;
+  $this->RB4 = $RB4;
+  $this->RB5 = $RB5;
   $this->date = $date;
 }
 
@@ -18,10 +28,15 @@ public function insert(){
     $pdo = $config->Connect();
     $name = $this->name;
     $rate = $this->rate;
+    $RB1 = $this->RB1;
+    $RB2 = $this->RB2;
+    $RB3 = $this->RB3;
+    $RB4 = $this->RB4;
+    $RB5 = $this->RB5;
     $date = $this->date;
-    $sql = "INSERT INTO `survey`( `name`, `rate`, `date`) VALUES (?,?,?)";
+    $sql = "INSERT INTO `survey`( `name`, `rate`, `A1`, `A2`, `A3`, `A4`, `A5`, `date`) VALUES (?,?,?,?,?,?,?,?)";
     $data = $pdo->prepare($sql);
-    $data->execute(["$name","$rate","$date"]);
+    $data->execute(["$name","$rate","$RB1","$RB2","$RB3","$RB4","$RB5","$date"]);
     // $results = $data->fetchAll(PDO::FETCH_ASSOC);
 
   //   echo '<table class="table ">';
@@ -43,7 +58,7 @@ public function insert(){
   // echo'</table>';
 
 
-    
+
   }
 }
 ?>
